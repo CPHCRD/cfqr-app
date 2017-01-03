54db92024c9d900a929240f8b8c856b5328bfd58
Jonathan Argentiero <jonathan.argentiero@gmail.com>
2017-01-03 22:54:10 +0100

    switch print button to iconbutton

--- app/components/Print.js
+++ app/components/Print.js
@@ -1,22 +1,26 @@
 // @flow
 import React, { Component, PropTypes } from 'react';
 import FontIcon from 'material-ui/FontIcon';
-import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
+
+import { connect } from '../actions';
 
 class Print extends Component {
 
   static propTypes = {
+    i18n: PropTypes.func,
     style: PropTypes.instanceOf(Object)
   };
 
   render() {
-    return (<FlatButton
-      onTouchTap={() => { window.print(); }}
-      secondary={true}
-      icon={<FontIcon className="material-icons">print</FontIcon>}
-      style={this.props.style}
-    />);
+    const { i18n } = this.props;
+
+    return (
+      <IconButton tooltip={i18n('print')} onTouchTap={() => { window.print(); }} style={this.props.style}>
+        <FontIcon className="material-icons">print</FontIcon>
+      </IconButton>
+    );
   }
 }
 
-export default Print;
+export default connect(Print);
